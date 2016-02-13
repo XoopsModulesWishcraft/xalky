2ed9bf78c87fbb37b39107fd375d2ddfc8b51a78
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2016-02-13 01:48:26 +0000

    Pre-alpha xalky 1.02

--- XoopsModules/xalky/trunk/modules/xalky/lookups/class/domains.php
+++ XoopsModules/xalky/trunk/modules/xalky/lookups/class/domains.php
@@ -14,7 +14,7 @@
  * @author      Simon Antony Roberts <wishcraft@users.sourceforge.net>
  * @see			http://sourceforge.net/projects/xoops/
  * @see			http://sourceforge.net/projects/chronolabs/
- * @see			http://sourceforge.net/projects/chronolabsapi/
+ * @see			http://sourceforge.net/projects/chronolabsapis/
  * @see			http://labs.coop
  * @version     1.0.5
  * @since		1.0.1
